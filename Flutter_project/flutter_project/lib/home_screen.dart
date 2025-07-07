4df874b331440cb5daf8d0a4208d54b2540b383c
Gulshid Zada <gulshidzada4@gmail.com>
2025-07-07 05:51:47 -0700

    Setup of node.js express.js with flutter

---
+++ Flutter_project/flutter_project/lib/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        children: [
+          ElevatedButton(
+          onPressed: (){},
+          child: Text('CREATE')),
+
+          ElevatedButton(
+        onPressed: (){},
+        child: Text('READ')),
+
+          ElevatedButton(
+        onPressed: (){},
+        child: Text('UPDATE')),
+
+          ElevatedButton(
+        onPressed: (){},
+        child: Text('DELETE')),
+        ],
+      ),
+    );
+  }
+}
