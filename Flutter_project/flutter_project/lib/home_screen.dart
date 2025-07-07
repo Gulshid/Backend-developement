7ab4da5c228cba576f2d634bd1d673b53689fb0a
Gulshid Zada <gulshidzada4@gmail.com>
2025-07-07 06:10:19 -0700

    Create view is created

--- Flutter_project/flutter_project/lib/home_screen.dart
+++ Flutter_project/flutter_project/lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/Create_Screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,28 +12,28 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-        centerTitle: true,
-      ),
+      backgroundColor: Colors.blue,
+      appBar: AppBar(title: const Text('Home Screen'), centerTitle: true),
 
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ElevatedButton(
-          onPressed: (){},
-          child: Text('CREATE')),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CreateScreen()),
+              );
+            },
+            child: Text('CREATE'),
+          ),
 
-          ElevatedButton(
-        onPressed: (){},
-        child: Text('READ')),
+          Center(child: ElevatedButton(onPressed: () {}, child: Text('READ'))),
 
-          ElevatedButton(
-        onPressed: (){},
-        child: Text('UPDATE')),
+          ElevatedButton(onPressed: () {}, child: Text('UPDATE')),
 
-          ElevatedButton(
-        onPressed: (){},
-        child: Text('DELETE')),
+          ElevatedButton(onPressed: () {}, child: Text('DELETE')),
         ],
       ),
     );
